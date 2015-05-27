e2689a8e5e76e6e81fedd48b3a728aab7910ca32
w1mvy <w1mvypine@gmail.com>
2015-05-27 19:48:16 +0900

    Remove variables

--- variables.tf
+++ variables.tf
@@ -3,15 +3,3 @@ variable "secret_key" {}
 variable "region" {
   default = "ap-northeast-1"
 }
-
-variable "key_name" {
-  description = "key pair name to ssh connection instance"
-}
-
-variable "key_path" {
-  description = "key path to ssh connection instance"
-}
-
-variable "vpc_id" {
-  description = "vpc id"
-}
